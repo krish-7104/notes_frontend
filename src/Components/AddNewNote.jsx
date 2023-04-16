ee7f010bebcb83619fa4049f3a1d395f46fa8362
Krish Jotaniya <krishjotaniya71@gmail.com>
2023-04-16 21:36:22 +0530

    add note fetch api error solved

--- src/Components/AddNewNote.jsx
+++ src/Components/AddNewNote.jsx
@@ -54,7 +54,7 @@ const AddNewNote = (props) => {
         }
       } else {
         try {
-          const data = await fetch("http://localhost:5000/api/notes/addnote", {
+          const data = await fetch(`${baseApiLink}/notes/addnote`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
